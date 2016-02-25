67b31d352b2604f421022104ca0fc47dfd629f74
dpoddubko <dpoddubko@yandex.ru>
2016-02-25 10:03:59 +0200

    adding multithreading into DirDataImpl; refactoring DirTest

--- src/test/java/com/dmipoddubko/fileSystemStatistic/DirTest.java
+++ src/test/java/com/dmipoddubko/fileSystemStatistic/DirTest.java
@@ -3,36 +3,90 @@ package com.dmipoddubko.fileSystemStatistic;
 import com.dmipoddubko.fileSystemStatistic.dir.DirDataImpl;
 import com.dmipoddubko.fileSystemStatistic.folderData.FolderData;
 import com.dmipoddubko.fileSystemStatistic.service.FileSystemServiceImpl;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.LinkOption;
-import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+
 
 public class DirTest {
-    @Test
-    public void createDirTest() {
+    private static String rootPath = "C:\\TestFolder";
+    private final static int THREADS = 5;
+
+    @BeforeClass
+    public static void createDirTest() {
         DirDataImpl dirData = new DirDataImpl();
-        dirData.createDir("C:\\TestDirectory\\1\\2\\3\\4\\5\\6\\7\\8\\9\\10\\11\\12\\13\\14\\15\\16\\17\\18\\19\\20\\21\\22\\23\\24\\25\\26\\27\\28\\29");
-        dirData.createFiles("C:\\TestDirectory");
+        String path = dirData.buildPath(rootPath, 30);
+        dirData.createDir(path);
+        List<String> paths = dirData.dividePath(rootPath, 30, THREADS);
+        ExecutorService pool = Executors.newFixedThreadPool(THREADS);
+        List<Callable<Object>> tasks = new ArrayList<>();
+        try {
+            for (String p : paths) {
+                tasks.add(new Callable<Object>() {
+                    public Object call() throws Exception {
+                        dirData.createFiles(p);
+                        return null;
+                    }
+                });
+            }
+            pool.invokeAll(tasks);
+        } catch (InterruptedException e) {
+            new RuntimeException("Some error with creating files", e);
+        } finally {
+            pool.shutdown();
+        }
+
+    }
+
+
+        @AfterClass
+        public static void deleteDirTest() {
+            DirDataImpl dirData = new DirDataImpl();
+            List<String> paths = dirData.dividePath(rootPath, 30, THREADS);
+            ExecutorService pool = Executors.newFixedThreadPool(THREADS);
+            List<Callable<Object>> tasks = new ArrayList<>();
+            try {
+                for (String p : paths) {
+                    tasks.add(new Callable<Object>() {
+                        public Object call() throws Exception {
+                            dirData.delDir(p);
+                            return null;
+                        }
+                    });
+                }
+                pool.invokeAll(tasks);
+            } catch (InterruptedException e) {
+                new RuntimeException("Some error with creating files", e);
+            } finally {
+                pool.shutdown();
+            }
+        }
+
+    @Test
+    public void fileExistTest() {
         try {
             long countDir = Files.find(
-                    Paths.get("C:\\TestDirectory"), 35,
+                    Paths.get(rootPath), 35,
                     (path, attributes) -> attributes.isDirectory()
             ).count();
             assertEquals(30, countDir);
 
             long countFiles = Files.find(
-                    Paths.get("C:\\TestDirectory"), 35,
+                    Paths.get(rootPath), 35,
                     (path, attributes) -> attributes.isRegularFile()
             ).count();
-            assertEquals(499800, countFiles);
+            assertEquals(499950, countFiles);
             System.out.println(countDir);
             System.out.println(countFiles);
         } catch (IOException e) {
@@ -44,22 +98,11 @@ public class DirTest {
     public void tableTest() {
         FileSystemServiceImpl systemService = new FileSystemServiceImpl();
         systemService.getFileDAO().create();
-        systemService.index("C:\\TestDirectory");
+        systemService.index(rootPath);
         List<FolderData> list = systemService.getFileDAO().read();
-        assertEquals(499830, list.size());
+        assertEquals(499980, list.size());
         systemService.getFileDAO().clean();
         list = systemService.getFileDAO().read();
         assertEquals(0, list.size());
-    }
-
-    @Test
-    public void deleteDirTest() {
-        DirDataImpl dirData = new DirDataImpl();
-        String s = "C:\\DirectoryForTest";
-        Path path = Paths.get(s);
-        assertTrue(Files.exists(path, LinkOption.NOFOLLOW_LINKS));
-        dirData.delDir("C:\\DirectoryForTest");
-        assertTrue(Files.notExists(path));
-
     }
 }
--- src/test/java/com/dmipoddubko/fileSystemStatistic/dir/DirDataImpl.java
+++ src/test/java/com/dmipoddubko/fileSystemStatistic/dir/DirDataImpl.java
@@ -4,34 +4,64 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.List;
 
+import static java.nio.file.FileVisitOption.FOLLOW_LINKS;
 import static java.nio.file.FileVisitResult.CONTINUE;
-import static org.apache.commons.io.FileUtils.deleteQuietly;
 
 public class DirDataImpl implements DirData {
-    private final static int NUMBER = 3333;
+    private final static int NUMBER = 3334;
 
     public void createDir(String path) {
         File file = new File(path);
         file.mkdirs();
     }
 
+    public String buildPath(String path, int depth) {
+        StringBuilder prepPath = new StringBuilder().append(path);
+        for (int i = 1; i < depth; i++)
+            prepPath = prepPath.append(File.separator).append(i);
+        return prepPath.toString();
+    }
+
+    public List<String> dividePath(String path, int depth, int treads) {
+        List<String> paths = new ArrayList<>();
+        StringBuilder prepPath = new StringBuilder().append(path);
+        paths.add(prepPath.toString());
+        int num = depth / treads;
+        for (int i = 1; i < depth; i++) {
+            prepPath = prepPath.append(File.separator).append(i);
+        if (i%num==0) paths.add(prepPath.toString());
+        }
+        return paths;
+    }
+
     public void createFiles(String path) {
         CreateFileVisitor fileVisitor = new CreateFileVisitor();
+        EnumSet<FileVisitOption> opts = EnumSet.of(FOLLOW_LINKS);
         try {
-            Files.walkFileTree(Paths.get(path), fileVisitor);
+            Files.walkFileTree(Paths.get(path), opts, 6, fileVisitor);
         } catch (IOException e) {
-            throw new RuntimeException("Folder error createFiles.", e);
+            throw new RuntimeException("Some error with creating files in folder.", e);
         }
     }
 
     public void delDir(String path) {
-        deleteQuietly(Paths.get(path).toFile());
+        DeleteFileVisitor fileVisitor = new DeleteFileVisitor();
+        EnumSet<FileVisitOption> opts = EnumSet.of(FOLLOW_LINKS);
+        try {
+            Files.walkFileTree(Paths.get(path), opts, 6, fileVisitor);
+        } catch (IOException e) {
+            throw new RuntimeException("Some error with deleting files.", e);
+        }
     }
 
     public static File prepareFile(StringBuilder sb, String s) {
         return new File(new StringBuilder().append(sb).append(s).toString());
     }
+
 
     public static class CreateFileVisitor extends SimpleFileVisitor<Path> {
         public FileVisitResult visitFile(Path file, BasicFileAttributes attr) {
@@ -59,6 +89,37 @@ public class DirDataImpl implements DirData {
                     avi.createNewFile();
                 } catch (IOException e) {
                     throw new RuntimeException("Some error with creating files in folder.", e);
+                }
+            }
+            return CONTINUE;
+        }
+
+        public FileVisitResult visitFileFailed(Path file, IOException exc) {
+            return CONTINUE;
+        }
+    }
+
+    public static class DeleteFileVisitor extends SimpleFileVisitor<Path> {
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attr) {
+            return CONTINUE;
+        }
+
+        public FileVisitResult postVisitDirectory(Path dir, IOException exc) {
+            for (int i = 1; i < NUMBER; i++) {
+                StringBuilder sb = new StringBuilder().append(dir).append(File.separator).append("file").append(i).append(".");
+                File txt = new File(new StringBuilder().append(sb).append("txt").toString());
+                File xml = new File(new StringBuilder().append(sb).append("xml").toString());
+                File pdf = new File(new StringBuilder().append(sb).append("pdf").toString());
+                File xls = new File(new StringBuilder().append(sb).append("xls").toString());
+                File avi = new File(new StringBuilder().append(sb).append("avi").toString());
+                try {
+                    Files.deleteIfExists(txt.toPath());
+                    Files.deleteIfExists(xml.toPath());
+                    Files.deleteIfExists(xls.toPath());
+                    Files.deleteIfExists(pdf.toPath());
+                    Files.deleteIfExists(avi.toPath());
+                } catch (IOException e) {
+                    throw new RuntimeException("Some error with deleting files.", e);
                 }
             }
             return CONTINUE;
