ade169fd9b1a64bb811b0bd9add9b8e9a7feb707
dpoddubko <dpoddubko@yandex.ru>
2016-03-23 14:13:51 +0200

    added folderDataMapperTest() into FileDAOTest

--- src/test/java/com/dmipoddubko/fileSystemStatistic/FileDAOTest.java
+++ src/test/java/com/dmipoddubko/fileSystemStatistic/FileDAOTest.java
@@ -1,12 +1,20 @@
 package com.dmipoddubko.fileSystemStatistic;
 
 import com.dmipoddubko.fileSystemStatistic.dao.FileDAOImpl;
+import com.dmipoddubko.fileSystemStatistic.folderData.FolderData;
 import org.junit.Test;
+import org.mockito.Mockito;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.*;
+import static org.testng.Assert.assertEquals;
 
 public class FileDAOTest {
     private ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
@@ -18,5 +26,24 @@ public class FileDAOTest {
         fileDAO.setJdbcTemplate(jdbcTemplate);
         fileDAO.read();
         verify(jdbcTemplate, times(1)).query(eq("SELECT * FROM directory"), any(FileDAOImpl.FolderDataMapper.class));
+    }
+
+    @Test
+    public void folderDataMapperTest() throws SQLException {
+        FileDAOImpl.FolderDataMapper mapper = new FileDAOImpl.FolderDataMapper();
+        ResultSet rs = mock(ResultSet.class);
+        Mockito.when(rs.getInt("id")).thenReturn(1);
+        Mockito.when(rs.getString("name")).thenReturn("testFile1.txt");
+        Mockito.when(rs.getString("path")).thenReturn("C:\\TestDirectory");
+        Mockito.when(rs.getString("type")).thenReturn("file");
+        Mockito.when(rs.getLong("size")).thenReturn((long) 123);
+        FolderData fd = mapper.mapRow(rs, 1);
+        assertNotNull(fd);
+        assertTrue(fd instanceof FolderData);
+        assertEquals(fd.getId(), rs.getInt("id"));
+        assertEquals(fd.getName(), rs.getString("name"));
+        assertEquals(fd.getPath(), rs.getString("path"));
+        assertEquals(fd.getType(), rs.getString("type"));
+        assertEquals(fd.getSize(), rs.getLong("size"));
     }
 }
